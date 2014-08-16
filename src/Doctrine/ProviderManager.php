ade39b2c55267e36b2fdda0f6c979f0f6dc1be36
Ujjwal Ojha <bring2uo@gmail.com>
2014-08-16 14:44:54 +0545

    Created doctrine manager

---
+++ src/Doctrine/ProviderManager.php
@@ -0,0 +1,31 @@
+<?php
+namespace Hrevert\OauthClient\Doctrine;
+
+use Hrevert\OauthClient\Manager\ProviderManagerInterface;
+use Doctrine\Common\Persistence\ObjectRepository;
+
+class ProviderManager implements ProviderManagerInterface
+{
+    /**
+     * @var ObjectRepository
+     */
+    protected $repository; 
+    
+    /**
+     * Constructor
+     *
+     * @param ObjectRepository $repository
+     */
+    public function __construct(ObjectRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * {@inheritDoc}
+     */    
+    public function findByName($name)
+    {
+        return $this->repository->findOneBy(['name' => $name);
+    }    
+}
---
+++ src/Doctrine/UserProviderManager.php
@@ -0,0 +1,67 @@
+<?php
+namespace Hrevert\OauthClient\Doctrine;
+
+use Hrevert\OauthClient\Manager\UserProviderManagerInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\Persistence\ObjectRepository;
+use Hrevert\OauthClient\Model\ProviderInterface;
+use Hrevert\OauthClient\Model\UserInterface;
+use Hrevert\OauthClient\Model\UserProviderInterface;
+
+class UserProviderManager implements UserProviderManagerInterface
+{
+    /**
+     * @var ObjectManager
+     */
+    protected $objectManager;
+
+    /**
+     * @var ObjectRepository
+     */
+    protected $repository;
+
+    /**
+     * Constructor
+     *
+     * @param ObjectManager $objectManager
+     * @param string $class
+     */
+    public function __construct(ObjectManager $objectManager, $class)
+    {
+        $this->objectManager = $objectManager;
+        $this->repository = $objectManager->getRepository($class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function findByProviderUid($providerUid, ProviderInterface $provider)
+    {
+        return $this->repository->findOneBy(['providerUid' => $providerUid, 'provider' => $provider]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function findByUserAndProvider(UserInterface $user, ProviderInterface $provider)
+    {
+        return $this->repository->findOneBy(['user' => $user, 'provider' => $provider]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function findByUser(UserInterface $user)
+    {
+        return $this->repository->findBy(['user' => $user]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function insert(UserProviderInterface $userProvider)
+    {
+        $this->objectManager->persist($userProvider);
+        $this->objectManager->flush();
+    }
+}
